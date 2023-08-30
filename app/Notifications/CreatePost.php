3ff0314c8d50ab5201529368c093a198326a78e1
mn0unreal <mnfs2050@gmail.com>
2023-08-30 12:42:40 +0300

    Update CreatePost.php

--- app/Notifications/CreatePost.php
+++ app/Notifications/CreatePost.php
@@ -19,15 +19,7 @@ class CreatePost extends Notification
     {
         return ['mail'];
     }
-
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
+    
     public function toArray($notifiable)
     {
         return [
