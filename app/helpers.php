3e422e5ce05e32f1c10bd8689191420863c43063
mn0unreal <mnfs2050@gmail.com>
2023-08-31 14:17:26 +0300

    Helpers
    Create Helpers functions

---
+++ app/helpers.php
@@ -0,0 +1,30 @@
+<?php
+//
+//Defining functions in your helpers class is the easy part, although, there are a few caveats. All of the Laravel helper files are wrapped in a check to avoid function definition collisions:
+//
+//if (! function_exists('env')) {
+//    function env($key, $default = null) {
+//        // ...
+//    }
+//}
+//This can get tricky, because you can run into situations where you are using a function definition that you did not expect based on which one was defined first.
+//
+//I prefer to use function_exists checks in my application helpers, but if you are defining helpers within the context of your application, you could forgo the function_exists check.
+//
+//By skipping the check, you’d see collisions any time your helpers are redefining functions, which could be useful.
+//
+//In practice, collisions don’t tend to happen as often as you’d think, and you should make sure you’re defining function names that aren’t overly generic. You can also prefix your function names to make them less likely to collide with other dependencies.
+
+use Illuminate\Support\Facades\Auth;
+
+if (! function_exists('userName')) {
+    function userName(){
+        return Auth::User()->name;
+    }
+}
+
+
+
+function userMail(){
+    return Auth::User()->email;
+}
